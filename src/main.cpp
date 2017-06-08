d992cb6feb12bcf1214c09744b841bfe1a6ad65f
Francisco José Fajardo Toril <frannnlol@gmail.com>
2017-06-08 19:36:49 +0200

    Drag and drop file to the window

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+#include <GL/glew.h>
+#define GLFW_DLL // Windows compatibility
 #include <GLFW/glfw3.h>
+
+void drop_callback(GLFWwindow* window, int count, const char** paths)
+{
+    int i;
+    for (i = 0;  i < count;  i++){
+        //handle_dropped_file(paths[i]);
+        std::cout << paths[i];
+    }
+    std::cout << std::endl;
+}
 
 int main( int argc, char** argv )
 {
@@ -8,8 +21,13 @@ int main( int argc, char** argv )
   if (!glfwInit())
     return 1;
 
+  /* Window options */
+  glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 ); // OpenGL 3.3.
+  glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
   /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  window = glfwCreateWindow(800, 600, "Face Expression Detector",NULL, NULL);
   if (!window){
     glfwTerminate();
     return 1;
@@ -17,6 +35,9 @@ int main( int argc, char** argv )
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+
+  /* Set callback functions */
+  glfwSetDropCallback(window, drop_callback);
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
@@ -28,7 +49,8 @@ int main( int argc, char** argv )
     glfwSwapBuffers(window);
 
     /* Poll for and process events */
-    glfwPollEvents();
+    //glfwPollEvents();
+    glfwWaitEvents();
   }
 
   glfwTerminate();
