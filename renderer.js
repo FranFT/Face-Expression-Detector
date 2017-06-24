3cbe9dd732e79b8a6aae89a1483e24b1da9d3e5e
Francisco José Fajardo Toril <frannnlol@gmail.com>
2017-06-24 10:33:17 +0200

    Plotting Face into the application canvas

--- renderer.js
+++ renderer.js
@@ -32,8 +32,29 @@ document.getElementById( 'selectImageButton' ).addEventListener( 'click', () => 
 });
 
 // Renderer process events.
+// Events which receive an image path and face coords and draws the face.
 ipcRenderer.on('faceInfo', (event, message) => {
-  console.log(message);
+  // Getting (x,y) face coordinates.
+  const coords = message[1].split(";")[0].split(",").map( function( item ){
+    return parseInt( item, 10 );
+  });
+  // Getting face area width and height.
+  const faceArea = message[1].split(";")[1].split(",").map( function( item ){
+    return parseInt( item, 10 );
+  });
+
+  // Plotting the face into the canvas.
+  const ctx = document.getElementById( 'imageArea' ).getContext( '2d' );
+  var img = new Image();
+  img.onload = function(){
+    console.log( img );
+    ctx.drawImage( img,
+      coords[0] - 50, coords[1] - 50, faceArea[0] + 50, faceArea[1] + 50,
+      0, 0, 500, 500 // Canvas coords.
+    );
+    ctx.stroke();
+  }
+  img.src = message[0];
 });
 
 ipcRenderer.on('logMsg', (event, message) =>{
