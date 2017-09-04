5f75a4c39fefd64b20dd39ad90b5e81292f3fe62
Francisco José Fajardo Toril <frannnlol@gmail.com>
2017-09-04 19:41:22 +0200

    Run script update

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
 # Building Node JS modules and electron.
-npm install
+#npm install
+#npm start
+if [ ! -d "build/" ];then
+  mkdir build
+  cd build
+  cmake .. && make -j2
+  cd ..
+fi
 npm start
